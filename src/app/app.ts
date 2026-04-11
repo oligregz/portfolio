6e50c82d6c7a442b95bc0c382a7d30483c51ac90
oligregz <gregorysak90@gmail.com>
2026-04-11 20:40:21 -0300

    faet(ui): implements hero component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './features/portfolio/components/hero.component';
+import { PORTFOLIO_DATA } from './core/data/portfolio.data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [HeroComponent],
+  template: `
+    <main class="min-h-screen bg-bg-main selection:bg-primary selection:text-white">
+      <app-hero [data]="portfolioData()"></app-hero>
+    </main>
+  `
 })
 export class App {
-  protected readonly title = signal('portfolio');
+  portfolioData = signal(PORTFOLIO_DATA);
 }
